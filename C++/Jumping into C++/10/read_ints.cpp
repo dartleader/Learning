8cf29bf30e080b13053cc6ff92fca10cc5c49341
Ivan Occam <ivan.occam@gmail.com>
2017-12-09 15:50:32 -0600

    Initial commit.

---
+++ C++/Jumping into C++/10/read_ints.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	int values[100];
+	for (int i = 0;i<100;i++)
+	{
+		cout << "Enter value " << i << ": ";
+		cin >> values[i];
+	}
+}
