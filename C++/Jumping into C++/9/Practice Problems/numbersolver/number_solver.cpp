13c2e177cad847ebc0c4d3488d50a5916a7a8c70
Ivan Occam <ivan.occam@gmail.com>
2017-12-09 03:19:13 -0600

    Functional number guessing program.

--- C++/Jumping into C++/9/Practice Problems/numbersolver/number_solver.cpp
+++ C++/Jumping into C++/9/Practice Problems/numbersolver/number_solver.cpp
@@ -3,34 +3,56 @@
 #include <iostream>
 #include <cstdlib>
 
+//Declare namespace.
 using namespace std;
 
 //Declare variables
-int secret_number;
-int top_guess = 100;				//Set top bound for guesses.
-int bottom_guess = 0;				//Set bottom bound for guesses.
-int guess = 50;					//Actual guess submitted to program.
+int secret_number;				//The secret number we are trying to guess.
+int top_guess = 100;				//Defines the top bound for guesses.
+int bottom_guess = 0;				//Defines the bottom bound for guesses.
+int guess;					//Actual guess submitted to program.
 int tries = 0;					//Keeps track of how many tries the program takes.
+int guess_tries = 0;				//Variable for how many tries random guessing takes.
+int split_tries = 0;				//Variable for how many tries split guessing takes.
 bool correct_guess = false;			//Turns true upon guessing correctly.
+bool splitting_method = false;			//Variable to signal if we are using the splitting method.
 
 //Prototype functions
-int randRange(int low, int high);
-int check_guess(int guess);
-void set_guess(int guess);
-int get_guess();
+int randRange(int low, int high);		//Function to compute a random number within a desired range.
+int check_guess(int guess);			//Function to check if our guess equals the secret_number.
 
 int main()
 {
+	//Call our functions to generate the target random number.
 	srand(time(NULL));			//Seed pseudo-random number generation.
 	secret_number = randRange(1,100);	//Call our randRange function to pick a pseudo-random number between 1 and 100.
 
+	//Begin functions to determine the random number.
+	//Function 1, guessing completely at random:
 	while(correct_guess == false)
 		{
-			check_guess(guess);
-			tries++;
-			get_guess();
+			guess = randRange(1,100);	//Call our randRange function to pick a pseudo-random number between 1 and 100 as a guess.
+			check_guess(guess);		//Call the check_guess function to check the guess.
 		}
-	cout << "Took " << tries << " tries to guess the correct number of " << guess << "\n";	//Displays final try count before exiting.
+	guess_tries=tries;				//Set the number of tries using random guessing counter equal to the number of tries it took.
+
+	//Reset the correct_guess variable to false and tries back to zero for new method.
+	correct_guess=false;
+	tries = 0;
+
+	//Now that we are switching to the splitting method, declare that.
+	splitting_method = true;
+
+	//Function 2, guessing using the splitting method.
+	while(correct_guess == false)
+		{
+			check_guess((bottom_guess+top_guess)/2);
+		}
+	split_tries = tries;				//Set the number of tries using split guessing equal to the number of tries it took.
+
+	//Displays final try count before exiting.
+	cout << "Took " << guess_tries << " random guesses to guess the correct number of " << secret_number << ".\n";				//Display final count of random guess method.
+	cout << "Took " << split_tries << " guesses to guess the correct number of " << secret_number << " using the splitting method.\n";	//Display final count of splitting method.
 }
 
 int randRange(int low, int high)
@@ -41,32 +63,29 @@ int randRange(int low, int high)
 
 int check_guess(int guess)
 {
-	if(guess == secret_number)
+	if(guess == secret_number)	//Guess is exactly correct.
 		{
 			cout << "Correct! The secret number is " << secret_number << "!\n";
 			correct_guess = true;	//Mark the guess as correct!
 		}
-	else if(guess >= secret_number)
+	else if(guess >= secret_number)	//Guess is too high.
 		{
 			cout << "You guessed " << guess << "! Your guess is too high!\n";
-			set_guess(top_guess+bottom_guess/2);
-			top_guess=guess-1;
+			if(splitting_method == true)	//Check if we are using the split method; if we are, modify the bounds of the search.
+			{
+				cout << "Changing top boundary of guess to " << guess << "\n";
+				top_guess=guess;
+			}
 		}
-	else if(guess <= secret_number)
+	else if(guess <= secret_number)	//Guess is too low.
 		{
 			cout << "You guessed " << guess << "! Your guess is too low!\n";
-			set_guess(top_guess+bottom_guess/2);
-			bottom_guess=guess+1;
+			if(splitting_method == true)	//Check if we are using the split method; if we are, modify the bounds of the search.
+			{
+				cout << "Changing bottom boundary of guess to " << guess << "\n";
+				bottom_guess=guess;
+			}
 		}
-	cout << secret_number << '\n';
-}
-
-void set_guess(int num)
-{
-	guess = num;	
-}
-
-int get_guess()
-{
-	return guess;
+	tries++;				//Increment the count of tries by 1.
+	cout << secret_number << '\n';		//Print secret_number for debugging.
 }
