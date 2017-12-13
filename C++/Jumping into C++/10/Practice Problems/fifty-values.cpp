719967c75562bb093faa2d7f3c4dfb769cdada78
Ivan Occam <ivan.occam@gmail.com>
2017-12-12 18:53:47 -0600

    Highest, lowest functions functional.

--- C++/Jumping into C++/10/Practice Problems/fifty-values.cpp
+++ C++/Jumping into C++/10/Practice Problems/fifty-values.cpp
@@ -15,6 +15,7 @@ int assignValues(int num);
 int randRange(int low,int high);
 void printvalues(int num);
 int getHighest(int array[]);
+int getLowest(int array[]);
 
 int main()
 {
@@ -23,7 +24,13 @@ int main()
 	printvalues(50);		//Print our values out
 
 	//Functions to do work on the array go here
-	getHighest(valuelist);
+	highestvalue=getHighest(valuelist);
+	lowestvalue=getLowest(valuelist);
+
+
+	//Print values we have calculated
+	cout << "Highest value : " << highestvalue << "\n";
+	cout << "Lowest value : " << lowestvalue << "\n";
 }
 
 int assignValues(int num)
@@ -49,5 +56,28 @@ void printvalues(int num)
 
 int getHighest(int array[])
 {
-	cout << "Called getHighest()\n";
+	int highest=array[0];			//Initialize highest variable as the first array element
+	//Check each array element; if it is higher than highest, assign its value to highest
+	for (int i=0;i<50;i++)
+	{
+		if(array[i]>highest)
+		{
+			highest=array[i];	
+		}
+	}
+	return highest;
+}
+
+int getLowest(int array[])
+{
+	int lowest=array[0];			//Initialize lowest variable as the first array element
+	//Check each array element; if it is lower than lowest, assign its value to lowest
+	for (int i=0;i<50;i++)
+	{
+		if(array[i]<lowest)
+		{
+			lowest=array[i];	
+		}
+	}
+	return lowest;
 }
