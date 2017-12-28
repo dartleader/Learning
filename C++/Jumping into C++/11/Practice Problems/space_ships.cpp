7bb0c309b69f95f88702ca72c1a553202d9553c1
Ivan Occam <ivan.occam@gmail.com>
2017-12-28 16:25:28 -0600

    Declares one ship as debug.

--- C++/Jumping into C++/11/Practice Problems/space_ships.cpp
+++ C++/Jumping into C++/11/Practice Problems/space_ships.cpp
@@ -6,20 +6,53 @@ using namespace std;
 //Structure for the spaceships
 struct SpaceShip
 {
-	int x_position=0;
-	int y_position=0;
+	int x_position;
+	int y_position;
+	string name;
 };
 
 //Initialize variables
 int ship_count = 2;	//How many ships do we want in total?
-SpaceShip ships[2];	//Array to hold the spaceships
+SpaceShip ship_1;	//Array to hold the spaceships
+SpaceShip temp;
+
+//Prototype functions
+SpaceShip getSpaceShip();
+//void printSpaceShip(SpaceShip ship);
+
+//SpaceShip getSpaceShip(int ship);
 
 int main()
 {
-	cout << "How many ships would you like to generate?\n";
-	cin >> ship_count;
-	for(int i=0;i<ship_count;i++)
+	//cout << "How many ships would you like to generate?\n";
+	//cin >> ship_count;
+	
+	//Create spaceship
+	SpaceShip ship_1;
+	ship_1.name = "Tyko";
+	ship_1.x_position=5;
+	ship_1.y_position=7;
+	/*for(int i=0;i<ship_count;i++)
 	{
-		//getSpaceShip(ships[i]);
-	}
+		cout << "Generating spaceship!\n";
+		SpaceShip temp = getSpaceShip();
+	}*/
+	cout << "Name : " << ship_1.name << "\n";
+	cout << "X position : " << ship_1.x_position << "\n";
+	cout << "Y position : " << ship_1.y_position << "\n";
+	//printSpaceShip(ship_1);
+}
+
+/*printSpaceShip(ship){
+	cout << ship.name;
+	cout << ship.x_position;
+	cout << ship.y_position;
+}*/
+
+SpaceShip getSpaceShip()
+{
+	SpaceShip temp;
+	temp.x_position;
+	temp.y_position;
+	return temp;
 }
