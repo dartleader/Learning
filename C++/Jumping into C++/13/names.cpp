ce7d82ae9af442c294f2e316fb9c79715e6a5095
Ivan Occam <ivan.occam@gmail.com>
2018-01-03 15:19:00 -0600

    Initial commit for exercise 1.

---
+++ C++/Jumping into C++/13/names.cpp
@@ -0,0 +1,16 @@
+//Write a function that prompts the user to enter his or her first and last name, as two separate values. This function should return both values to the caller via additional pointer (or reference) parameters that are passed to the function. Try doing this first with pointers and then with references.
+#include <iostream>
+
+using namespace std;
+
+//Initialize Variables
+string first_name;
+string last_name;
+
+int main()
+{
+	cout << "What is your first name?\n";	
+	cin >> first_name;
+	cout << "What is your last name?\n";	
+	cin >> last_name;
+}
