922ac86baa21f8cb851a7a0ec82f4cc8f59afceb
dartleader <ivan.occam@gmail.com>
2018-06-25 14:28:09 -0600

    Added list of C terms, keywords, operators. Added Makefile and exercises.

--- C/ex1.c
+++ C/ex1.c
@@ -8,5 +8,12 @@ int main(int argc, char *argv[])
 		//this is also a comment
 		printf("You are %d miles away.\n",distance);
 
+		//Extra credit stuff:
+		printf("Line one of text.\n");
+		printf("Line two of text.\n");
+		printf("Line three of text.\n");
+		printf("Line four of text.\n");
+		printf("Line five of text.\n");
+
 		return 0;
 }
